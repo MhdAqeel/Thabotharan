143ec8e4583542b038475b735fad369346efb447
MhdAqeel <mohamedaqeel1130@gmail.com>
2025-07-25 23:53:26 +0530

    testted all getter mehods and they are working

--- Line.java
+++ Line.java
@@ -11,57 +11,64 @@ public class Line{
 		this.end = end;		
 	}
 	
+	//getters
 	public Point getBegin(){
 		return this.begin;
-	}
-	public void setBegin(Point begin){
-		this.begin = begin;
 	}
 	public Point getEnd(){
 		return this.end;
 	}
-	public void setEnd(Point end){
-		this.end=end;
-	}
 	public int getBeginX(){
 		return this.getBegin().getX();
-	}
-	public void setBeginX(int x){
-		this.begin.setX(x);
 	}
 	public int getBeginY(){
 		return this.getBegin().getY();
 	}
-	public void setBeginY(int y){
-		this.begin.setY(y);
-	}
 	public int[] getBeginXY(){
 		return new int[] {this.getBeginX(),this.getBeginY()};
-	}
-	public void setBeginXY(int x , int y){
-		this.begin = new Point(x,y);
 	}
 	public int getEndX(){
 		return this.getEnd().getX();
 	}
-	public void setEndX(int x){
-		this.end.setX(x);
-	}
 	public int getEndY(){
 		return this.getEnd().getY();
-	}
-	public void setEndY(int y){
-		this.end.setY(y);
 	}
 	public int[] getEndXY(){
 		return new int[] {this.getEndX(),this.getEndY()};
 	}
+	public void setBegin(Point begin){
+		this.begin = begin;
+	}
+	
+	//setters
+	public void setEnd(Point end){
+		this.end=end;
+	}	
+	public void setBeginX(int x){
+		this.begin.setX(x);
+	}	
+	public void setBeginY(int y){
+		this.begin.setY(y);
+	}	
+	public void setBeginXY(int x , int y){
+		this.begin = new Point(x,y);
+	}
+	public void setEndX(int x){
+		this.end.setX(x);
+	}
+	public void setEndY(int y){
+		this.end.setY(y);
+	}
 	public void setEndXY(int x , int y){
 		this.end = new Point(x,y);
 	}
+
+	//toString 
 	public String toString(){
 		return"Line [ Begin = ( "+ this.getBeginX() +" , "+ this.getBeginY() +" ) , End = ( "+ this.getEndX() +" , "+ this.getEndY() +" ) ] ";
 	}
+
+	//find length Of A Line
 	public double getLength(){
 		return this.getBegin().distance(this.getEnd());
 	}
