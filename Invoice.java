4f5946862e3267eab2e7459fe0f7fcd7633f2df7
Mohamed Aqeel <mohamedaqeel1130@gmail.com>
2025-08-07 10:41:54 +0530

    super methods are added

--- Invoice.java
+++ Invoice.java
@@ -1,22 +1,23 @@
-public class Invoice{
+public class Invoice extends Customer{
 	private int id;
-	private Customer customer;
+	//private Customer customer;
 	private double amount;
 	
-	public Invoice(int id , Customer customer , double amount){
+	public Invoice(int cid , String name,int discount,int id, double amount){
+		super(cid , name , discount);
 		this.id = id;
-		this.customer = customer;
+		//this.customer = customer;
 		this.amount = amount;
 	}
 	public int getId(){
 		return this.id;
 	}
-	public Customer getCustomer(){
+	/*public Customer getCustomer(){
 		return this.customer;
 	}
 	public void setCustomer(Customer customer){
 		this.customer = customer;
-	}
+	}*/
 	public double getAmount(){
 		return this.amount;
 	}
@@ -24,19 +25,19 @@ public class Invoice{
 		this.amount = amount;
 	}
 	public int getCustomerId(){
-		return this.getCustomer().getId();
+		return super.getId();
 	}
 	public String getCustomerName(){
-		return this.getCustomer().getName();
+		return super.getName();
 	}
 	public int getCustomerDiscount(){
-		return this.getCustomer().getDiscount();
+		return super.getDiscount();
 	}
 	public double getAmountAfterDiscount(){
 		double dis = (this.amount /100)*getCustomerDiscount();
 		return (this.amount-dis);
 	}
 	public String toString(){
-	return "Invoice [ id = "+this.getId()+", customer = "+getCustomer().toString()+", amount = "+getAmountAfterDiscount()+" ]";
+	return "Invoice [ id = "+this.getId()+", customer = "+super.toString()+", amount = "+getAmountAfterDiscount()+" ]";
 	}
 }
--- TestInvoice.java
+++ TestInvoice.java
@@ -1,8 +1,9 @@
 public class TestInvoice{
 	public static void main (String [] args){
-		Customer c1 = new Customer (001 , "Aqeel" , 10);
-		Invoice i1 = new Invoice (100 , c1 , 1000.0);
+		//Customer c1 = new Customer (001 , "Aqeel" , 10);
+		Invoice i1 = new Invoice (001 , "Aqeel" , 10, 100, 1000.0);
 		
 		System.out.println(i1.toString());
+		System.out.println(i1.getCustomerName());
 	}
 }
