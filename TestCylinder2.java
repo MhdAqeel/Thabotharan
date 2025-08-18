03c94dcf2cfc53032f66f73f6f37c40c384031d3
DCS User <DCSUser@cn.lan>
2025-08-18 08:39:43 +0530

    This is the files from 18th of August

---
+++ TestCylinder2.java
@@ -0,0 +1,8 @@
+public class TestCylinder2{
+	public static void main (String [] args){
+		Circle c1 = new Cylinder (3.75,5.28);
+		System.out.println("radius is "+c1.getRadius());
+		//System.out.println("height is "+c1.getHeight());
+		System.out.println("color is "+c1.getColour());
+	}
+}
