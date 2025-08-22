51ab68ca363c9ee968fd18080300597ea72cfc5b
MhdAqeel <mohamedaqeel1130@gmail.com>
2025-08-22 21:59:38 +0530

    1

---
+++ Student.java
@@ -0,0 +1,25 @@
+public class Student extends Person{
+    private int numCourses = 0;
+    private String [] courses = {};      //a student can study upto maximum of 30 courses
+    private int [] grades = {};
+
+    public Student (String name , String adress) {
+        super(name , adress);
+    }
+
+    public String toString () {
+        return "";
+    }
+
+    public void addCourseGrade(String couese , int grade){
+
+    }
+
+    public void printGrades(){
+
+    }
+
+    public double getAverageGrade(){
+
+    }
+}
--- Teacher.java
+++ Teacher.java
@@ -1,3 +1,17 @@
-public class Teacher {
-    
+public class Teacher extends Person{
+    private int numCourses = 0; //a teacher can teach upto 5 courses
+    private String [] courses = {};
+
+    public Teacher (String name ,String address){
+        super(name, address);
+    }
+    public String toString (){
+        return "";
+    }
+    public boolean addCourse(String course){
+        return ;
+    }
+    public boolean removeCourse(String course){
+        return;
+    }
 }
