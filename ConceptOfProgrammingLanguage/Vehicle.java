6503491e19e6823a9f6ef1f8ad5f2da487296a9e
Rahuman Noor Mohamed Aqeel <mohamedaqeel1130@gmail.com>
2026-01-12 11:14:04 +0530

    Add Vehicle class with specified access modifiers

--- ConceptOfProgrammingLanguage/Vehicle.java
+++ ConceptOfProgrammingLanguage/Vehicle.java
@@ -1,19 +1,31 @@
-public class Vehicle{
+package ConceptOfProgrammingLanguage;
+
+// Parent class: Vehicle
+public class Vehicle {
+
+    // A unique engine ID, completely hidden from all classes
     private String engineID;
+
+    // Brand name accessible to everyone
     public String brandName;
+
+    // Fuel level, hidden from the general public but visible to subclasses
     protected double fuelLevel;
 
-    public Vehicle(String engineID,String brandName,double fuelLevel){
-        this.engineID=engineID;
-        this.brandName=brandName;
-        this.fuelLevel=fuelLevel;
+    // Constructor to initialize the Vehicle attributes
+    public Vehicle(String engineID, String brandName, double fuelLevel) {
+        this.engineID = engineID;
+        this.brandName = brandName;
+        this.fuelLevel = fuelLevel;
     }
 
-    public void startEngine(){
-        System.out.println("Engine started");
+    // Method visible to everyone
+    public void startEngine() {
+        System.out.println("Engine started.");
     }
 
-    protected void injectFuel(){
-       System.out.println("Injecting fuel");
+    // Method visible only to subclasses
+    protected void injectFuel() {
+        System.out.println("Injecting fuel.");
     }
 }
